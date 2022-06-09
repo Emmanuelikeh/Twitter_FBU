26656ee49eccf8ccf8fb66bcaa483a3d47fe2a5a
emmanuelikeh <tochukwuikye@gmail.com>
2022-06-08 22:05:31 -0700

    updated commit
    added relative time stamps to the displayed tweets

--- app/src/main/java/com/codepath/apps/restclienttemplate/TweetsAdapter.java
+++ app/src/main/java/com/codepath/apps/restclienttemplate/TweetsAdapter.java
@@ -1,6 +1,8 @@
 package com.codepath.apps.restclienttemplate;
 
 import android.content.Context;
+import android.text.format.DateUtils;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -13,7 +15,10 @@ import androidx.recyclerview.widget.RecyclerView;
 import com.bumptech.glide.Glide;
 import com.codepath.apps.restclienttemplate.models.Tweet;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.util.List;
+import java.util.Locale;
 
 public class TweetsAdapter extends RecyclerView.Adapter<TweetsAdapter.ViewHolder>{
 
@@ -50,6 +55,13 @@ public class TweetsAdapter extends RecyclerView.Adapter<TweetsAdapter.ViewHolder
         TextView tvBody;
         TextView tvScreenName;
         ImageView ivMediaUrl;
+        TextView tvTimeStamp;
+
+        private static final int SECOND_MILLIS = 1000;
+        private static final int MINUTE_MILLIS = 60 * SECOND_MILLIS;
+        private static final int HOUR_MILLIS = 60 * MINUTE_MILLIS;
+        private static final int DAY_MILLIS = 24 * HOUR_MILLIS;
+
 
         public ViewHolder(@NonNull View itemView) {
             super(itemView);
@@ -57,12 +69,49 @@ public class TweetsAdapter extends RecyclerView.Adapter<TweetsAdapter.ViewHolder
             tvBody = itemView.findViewById(R.id.tvBody);
             tvScreenName = itemView.findViewById(R.id.tvScreenName);
             ivMediaUrl = itemView.findViewById(R.id.ivMediaUrl);
+            tvTimeStamp = itemView.findViewById(R.id.tvTimeStamp);
 
 
         }
 
+        public String getRelativeTimeAgo(String rawJsonDate) {
+            String twitterFormat = "EEE MMM dd HH:mm:ss ZZZZZ yyyy";
+            SimpleDateFormat sf = new SimpleDateFormat(twitterFormat, Locale.ENGLISH);
+            sf.setLenient(true);
+
+            try {
+                long time = sf.parse(rawJsonDate).getTime();
+                long now = System.currentTimeMillis();
+
+                final long diff = now - time;
+                if (diff < MINUTE_MILLIS) {
+                    return "just now";
+                } else if (diff < 2 * MINUTE_MILLIS) {
+                    return "a minute ago";
+                } else if (diff < 50 * MINUTE_MILLIS) {
+                    return diff / MINUTE_MILLIS + " m";
+                } else if (diff < 90 * MINUTE_MILLIS) {
+                    return "an hour ago";
+                } else if (diff < 24 * HOUR_MILLIS) {
+                    return diff / HOUR_MILLIS + " h";
+                } else if (diff < 48 * HOUR_MILLIS) {
+                    return "yesterday";
+                } else {
+                    return diff / DAY_MILLIS + " d";
+                }
+            } catch (ParseException e) {
+                Log.i("TweetAdapter", "getRelativeTimeAgo failed");
+                e.printStackTrace();
+            }
+
+            return "";
+        }
+
+
+
         public void bind(Tweet tweet) {
             tvBody.setText(tweet.body);
+            tvTimeStamp.setText(getRelativeTimeAgo(tweet.createdAt));
             tvScreenName.setText(tweet.user.screenName);
             Glide.with(context).load(tweet.user.profileImageUrl).into(ivProfileImage);
             if(!tweet.mediaUrl.equals("")){
